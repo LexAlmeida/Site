b1741fa61fdf72e2c814f34dce819ba47914a1ab
alexalmeida <alexalmeida01207@gmail.com>
2025-12-03 15:17:11 -0300

    feat: Adicionado tema claro e Provider

--- meu-projeto-vite/src/App.tsx
+++ meu-projeto-vite/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter} from "react-router-dom"
 import { AppRoutes } from "./routes"
+import { ThemeProvider } from "@emotion/react"
+import { LightTheme } from "./shared/themes"
 function App() {
   return (
-    <>
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
       <AppRoutes/>
     </BrowserRouter>
       
-    </>
+    </ThemeProvider>
   )
 }
 
